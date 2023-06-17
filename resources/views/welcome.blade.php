f9bd4e5812261ef632b71c6a0d1f451ae77d9084
Kenn Secusana <kennsecusana@gmail.com>
2023-06-17 20:27:08 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-slate-700">
+        <h1 class="text-3xl text-white text-start py-7 px-9 font-bold">
+            Xplct
+          </h1>
+    </body>
+</html>
